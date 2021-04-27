8ce541a248294c06e9c235a0a4ec5dc4aed23267
immanual19 <immanuelsarker1@gmail.com>
2021-04-28 01:16:33 +0600

    6th

---
+++ aNearlyLuckyNumber.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    string number;
+    int count=0;
+    cin>>number;
+    
+    for(int i=0;i<number.length();i++){
+        if(number[i]=='4' || number[i]=='7'){
+            count++;
+        }
+    }
+    
+    if(count==4 || count==7){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    
+    return 0;
+}
